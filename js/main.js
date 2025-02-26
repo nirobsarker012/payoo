96e2b85d85729abd1927008fda0bb2cc2f8c61f1
nirobsarker012 <nirobsarker012@gmail.com>
2025-02-26 15:51:13 +0600

    new: login_page

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//catching button ID
+document
+  .querySelector("#login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Page for not reloads.
+    const phNumber = document.querySelector("#ph-number").value;
+    // adding pin number
+    const pinNumber = document.querySelector("#pin-number").value;
+    const convertedPin = parseInt(pinNumber); // Converted to pin string to number
+    if (phNumber.length === 11) {
+      if (convertedPin === 1234) {
+        window.location.href = "../home_page.html";
+      } else {
+        alert("Invalid Pin Number");
+      }
+    } else {
+      alert("Need Valid Account");
+    }
+  });
