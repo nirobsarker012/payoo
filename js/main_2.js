e9223c9e2e68d5856309b2e69e88e1a8447ea5b0
nirobsarker012 <nirobsarker012@gmail.com>
2025-02-27 20:26:00 +0600

    login-fixed

--- js/main_2.js
+++ js/main_2.js
@@ -1,23 +1,24 @@
-// document.querySelector("#add-btn").addEventListener("click", function (event) {
-//   event.preventDefault();
-//   //   Adding account Numbers
-//   const checkAccountNumber = getInputValuesById("account-no");
-//   const addAmount = getInputValuesById("amount");
-//   console.log(addAmount);
-//   //   Adding Pin Numbers
-//   const addPinNumbers = getInputValuesById("pin");
-//   //  ADDING MAIN_BALANCE
-//   const mainBalance = getInputInnerTextById("balance");
+document.querySelector("#add-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+  //   Adding account Numbers
+  const checkAccountNumber = document.querySelector("#account-no").value;
+  const addAmount = getInputValuesById("amount");
+  console.log(addAmount);
+  //   Adding Pin Numbers
+  const addPinNumbers = getInputValuesById("pin");
+  //  ADDING MAIN_BALANCE
+  const mainBalance = getInputInnerTextById("balance");
 
-//   //   Condition
-//   if (checkAccountNumber.length === 11) {
-//     if (addPinNumbers === 1234) {
-//       const sum = mainBalance + addAmount;
-//       document.getElementById("balance").innerText = sum;
-//     } else {
-//       alert(`Adding successfully`);
-//     }
-//   } else {
-//     alert(`Invalid account Number`);
-//   }
-// });
+  //   Condition
+  if (checkAccountNumber.length === 11) {
+    if (addPinNumbers === 1234) {
+      const sum = mainBalance + addAmount;
+      console.log(sum);
+      document.getElementById("balance").innerText = sum;
+    } else {
+      alert(`Adding successfully`);
+    }
+  } else {
+    alert(`Invalid account Number`);
+  }
+});
